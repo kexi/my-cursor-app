bd1555c141aa321c27a65ba0e2a23a6bb3a35fd5
Kei Nakayama <kei.of.nakayama@gmail.com>
2025-02-26 18:45:24 +0900

    Update welcome message to お久しぶり！

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
       }
     >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">こんにちは！</ThemedText>
+        <ThemedText type="title">お久しぶり！</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.welcomeContainer}>
